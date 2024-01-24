1f1fedad9f8af635ba4c67ac2773828fc4de0001
Kushal <ktatariya98@gmail.com>
2024-01-24 17:05:36 +0100

    too big for local running now - moving to cluster

--- ud_parser_edit.py
+++ ud_parser_edit.py
@@ -555,21 +555,3 @@ class ConlluUDParser:
                 if self.verbose:
                     print(f"Results are saved by the path: {output_path}")
 
-def parse(
-    tr_path: Optional[os.PathLike] = None,
-    va_path: Optional[os.PathLike] = None,
-    te_path: Optional[os.PathLike] = None,
-    dir_conllu_path: Optional[os.PathLike] = None,
-    language: Optional[str] = None,
-    save_path_dir: Optional[os.PathLike] = None,
-    shuffle: bool = False,
-    verbose: bool = False,
-) -> None:
-    converter = ConlluUDParser(shuffle, verbose)
-    converter.convert(
-        tr_path, va_path, te_path, dir_conllu_path, language, save_path_dir
-    )
-
-folder_path = "UD_English-EWT"
-
-parse(dir_conllu_path=folder_path)
